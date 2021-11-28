d7c79ce7f21927cad2a17e75b4cb01b1b56ff2b6
Erek Speed <melink14@gmail.com>
2021-11-28 17:04:21 +0900

    feat(warnings): Also fail lint when there are warnings

--- src/validatePrTitle.js
+++ src/validatePrTitle.js
@@ -1,13 +1,19 @@
+const load = require('@commitlint/load').default;
 const lint = require('@commitlint/lint').default;
 const config = require('../commitlint.config.js');
 
 module.exports = async function validatePrTitle(prTitle, rootDir) {
   console.log(rootDir);
-  const customConfig = await require(rootDir + '/commitlint.config.js');
+  console.log(prTitle);
+  const customConfig = await load(rootDir + '/commitlint.config.js');
+  console.log(customConfig.rules);
   const result = await lint(prTitle, customConfig.rules);
 
-  if (!result.valid) {
+  if (!result.valid || result.warnings.length > 0) {
     const errorMessages = result.errors.map((error) => error.message);
-    throw new Error(errorMessages.join('; '));
+    const warningMessages = result.warnings.map((warning) => warning.message);
+    throw new Error(
+      errorMessages.join('; ') + '; ' + warningMessages.join('; ')
+    );
   }
 };
